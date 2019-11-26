3eea52903a62b1af1d6257bd9bf8f42b185e40bc
helenecharlotte <helenerytgaard@gmail.com>
2019-11-26 19:23:12 +0100

    add option to correct for given treatment

--- R/hunt-fun-fast.R
+++ R/hunt-fun-fast.R
@@ -2,6 +2,7 @@ hunt.fun.fast <- function(dt, ## time, delta (0=censoring), A, X
                           X.vars=paste0("X", 1:4),
                           CR.as.censoring=FALSE,
                           stratify.CR=NULL,
+                          stratify.A=FALSE,
                           event=1, method.weight="KM",
                           A.vars=paste0("A", 1:10),
                           t0=0.5,
@@ -14,35 +15,50 @@ hunt.fun.fast <- function(dt, ## time, delta (0=censoring), A, X
 
     if (browse) browser()
 
-    #-- compute weights with KM:
-
-    if (method.weight=="KM") {
-        if (CR.as.censoring) {
-            dt[,is.event:=as.numeric(delta==event)]
-        }else{
-            dt[,is.event:=delta]
-        }
-        if (length(stratify.CR)==0) {
-            form <- formula(paste0("Hist(time, is.event)~ 1"))
-        } else{
-            form <- formula(paste0("Hist(time, is.event)~ ", stratify.CR))
-        }
-        reverse.km.fit <- prodlim(form, data=dt, reverse=TRUE)
-        ## resultat of predictSurvIndividual is sorted by (time, -status)
-        setorder(dt,time,-is.event)
-        dt[,wt:=(delta==event)*(time<=t0)/predictSurvIndividual(reverse.km.fit)]
-        ## back to original order
-        setorder(dt,id)
-    } else {
-        warning("only KM implemented")
-    }
-
-    #-- define real-valued outcome:
-    dt[, Y1:=wt*as.numeric(time<=t0)]
-
-    #-- run grf:
-
     ATE.list <- lapply(A.vars, function(a) {
+
+        #-- reset weights and define id variable:
+
+        dt[, id:=1:.N]
+        dt[, wt:=0]
+
+        if (browse) browser()
+
+        if (length(stratify.CR)>0 & stratify.A) {
+            stratify.CR.A <- paste0(stratify.CR, "+", a)
+        } else if (stratify.A) {
+            stratify.CR.A <- a
+        } else {
+            stratify.CR.A <- stratify.CR
+        }
+
+        #-- compute weights with KM:
+
+        if (method.weight=="KM") {
+            if (CR.as.censoring) {
+                dt[,is.event:=as.numeric(delta==event)]
+            }else{
+                dt[,is.event:=delta]
+            }
+            if (length(stratify.CR.A)==0) {
+                form <- formula(paste0("Hist(time, is.event)~ 1"))
+            } else{
+                form <- formula(paste0("Hist(time, is.event)~ ", stratify.CR.A))
+            }
+            reverse.km.fit <- prodlim(form, data=dt, reverse=TRUE)
+            ## resultat of predictSurvIndividual is sorted by (time, -status)
+            setorder(dt,time,-is.event)
+            dt[,wt:=(delta==event)*(time<=t0)/predictSurvIndividual(reverse.km.fit)]
+            ## back to original order
+            setorder(dt,id)
+        } else {
+            warning("only KM implemented")
+        }
+
+        #-- define real-valued outcome:
+        dt[, Y1:=wt*as.numeric(time<=t0)]
+
+        #-- run grf:
 
         grf.A <- causal_forest(
             model.matrix(~ -1 + .,
