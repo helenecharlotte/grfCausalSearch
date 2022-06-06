b43b83fceb06aadf73df6a7a115b97fe16bfd81e
Thomas Alexander Gerds <tag@biostat.ku.dk>
2022-06-06 08:44:28 +0200

    progress

---
+++ functions/lavaModel.R
@@ -0,0 +1,101 @@
+### lavaModel.R --- 
+#----------------------------------------------------------------------
+## Author: Thomas Alexander Gerds
+## Created: Apr  9 2022 (19:27) 
+## Version: 
+## Last-Updated: May 11 2022 (08:58) 
+##           By: Thomas Alexander Gerds
+##     Update #: 62
+#----------------------------------------------------------------------
+## 
+### Commentary: 
+## 
+### Change Log:
+#----------------------------------------------------------------------
+## 
+### Code:
+lavaModel <- function(event.times = c("T1","T2"),
+                      treatments,
+                      binary.covariates = NULL,
+                      normal.covariates = NULL,
+                      quadratic.covariates = NULL,
+                      scale = 1/100,
+                      treatment.effect,
+                      formula.list = NULL,
+                      randomized = FALSE,
+                      ...){
+    # empty latent variable model
+    requireNamespace("lava")
+    m <- lava::lvm()
+    # covariates
+    for (X in names(binary.covariates)) {
+        lava::distribution(m,X) <- lava::binomial.lvm()
+        lava::intercept(m,X) <- log(binary.covariates[[X]])
+    }
+    for (X in normal.covariates) lava::distribution(m,X) <- lava::normal.lvm()
+    for (X in quadratic.covariates) lava::transform(m,formula(paste0(X,"_2","~",X))) <- function(x){x^2}
+    # treatment variables
+    for (A in names(treatments)) {
+        lava::distribution(m,A) <- lava::binomial.lvm()
+        lava::intercept(m,A) <- log(treatments[[A]])
+    }
+    # initialize all latent event times with same scale
+    latent.times = sapply(event.times,function(x){paste0(x,"_",c("placebo_placebo","treated_placebo","placebo_treated","treated_treated"))})
+    for (R in c(latent.times,"C")) lava::distribution(m,R) <- lava::coxWeibull.lvm(scale = scale)
+    # change the scale according to treatment effects
+    lava::distribution(m,"T1_treated_treated") <- lava::coxWeibull.lvm(scale = scale * treatment.effect[["A1_T1"]]* treatment.effect[["A2_T1"]])
+    lava::distribution(m,"T1_treated_placebo") <- lava::coxWeibull.lvm(scale = scale * treatment.effect[["A1_T1"]])
+    lava::distribution(m,"T1_placebo_treated") <- lava::coxWeibull.lvm(scale = scale * treatment.effect[["A2_T1"]])
+    lava::distribution(m,"T2_treated_treated") <- lava::coxWeibull.lvm(scale = scale * treatment.effect[["A1_T2"]]* treatment.effect[["A2_T2"]])
+    lava::distribution(m,"T2_treated_placebo") <- lava::coxWeibull.lvm(scale = scale * treatment.effect[["A1_T2"]])
+    lava::distribution(m,"T2_placebo_treated") <- lava::coxWeibull.lvm(scale = scale * treatment.effect[["A2_T2"]])
+    # all other regressions
+    # all other regressions
+    for (f in formula.list) {
+        if ((timevar <- all.vars(f)[[1]]) %in% c("T1","T2")){
+            for(a in c("_treated_treated","_treated_placebo","_placebo_treated","_placebo_placebo")){
+                ## print(update(f,paste(paste0(timevar,a),"~.")))
+                lava::regression(m) = update(f,paste(paste0(timevar,a),"~."))
+            }
+        }else{
+            lava::regression(m) = f
+        }
+    }
+    # observed treatment
+    lava::transform(m,T1~T1_placebo_placebo+T1_treated_placebo+T1_placebo_treated+T1_treated_treated+A1+A2) = function(x){
+        x[["T1_placebo_placebo"]]*(x[["A1"]] == 0)*(x[["A2"]] == 0) +
+            x[["T1_treated_placebo"]]*(x[["A1"]] == 1)*(x[["A2"]] == 0) +
+            x[["T1_placebo_treated"]]*(x[["A1"]] == 0)*(x[["A2"]] == 1) +
+            x[["T1_treated_treated"]]*(x[["A1"]] == 1)*(x[["A2"]] == 1)
+    }
+    lava::transform(m,T2~T2_placebo_placebo+T2_treated_placebo+T2_placebo_treated+T2_treated_treated+A1+A2) = function(x){
+        x[["T2_placebo_placebo"]]*(x[["A1"]] == 0)*(x[["A2"]] == 0) +
+            x[["T2_treated_placebo"]]*(x[["A1"]] == 1)*(x[["A2"]] == 0) +
+            x[["T2_placebo_treated"]]*(x[["A1"]] == 0)*(x[["A2"]] == 1) +
+            x[["T2_treated_treated"]]*(x[["A1"]] == 1)*(x[["A2"]] == 1)
+    }
+    # event time outcome
+    m <- lava::eventTime(m,time~min(T1=1,T2=2,C=0),"event")
+    m
+}
+## lava::distribution(m,"A1_0") <- lava::constant.lvm(value=0)
+## lava::distribution(m,"A1_1") <- lava::constant.lvm(value=1)
+## u <- lavaModel(treatments = c("A1","A2"),
+               ## binary.covariates = paste0("X",1:3),
+               ## normal.covariates = "Z",
+               ## treatment.effect =list("A1_T1" = log(2), "A1_T2" = 0, "A2_T1" = 0, "A2_T2" = log(.5)),
+               ## formula.list = NULL);sim(u,4)
+
+## v <- lavaModel(seed = 11,horizon = 5,event.times = c("T1","T2"),
+               ## treatments = paste0("A",1:10),
+               ## binary.covariates = paste0("X",1:5),
+               ## normal.covariates = paste0("X",6:7),
+               ## treatment.effect = list("A1_T1" = log(2), "A1_T2" = 0,
+                                       ## "A2_T1" = 0, "A2_T2" = log(.5)),
+               ## formula.list =  list (T1 ~ f(X1,-.4) + f(X2,.3) + f(X6,-.1),
+                                     ## T2  ~ f(X1,-.1) + f(X2,.6) + f(X6,.1),
+                                     ## C ~ f(A1,.5)+ f(A3,.5) + f(X2,.6) + f(A6,.1),
+                                     ## A1 ~ f(X1,.4) + f(X6,.8)),n = 4000)
+## sim(v,4)
+######################################################################
+### lavaModel.R ends here
---
+++ functions/theTruth.R
@@ -0,0 +1,57 @@
+### theTruth.R --- 
+#----------------------------------------------------------------------
+## Author: Thomas Alexander Gerds
+## Created: Apr 10 2022 (09:18) 
+## Version: 
+## Last-Updated: Apr 29 2022 (09:07) 
+##           By: Thomas Alexander Gerds
+##     Update #: 64
+#----------------------------------------------------------------------
+## 
+### Commentary: 
+## 
+### Change Log:
+#----------------------------------------------------------------------
+## 
+### Code:
+theTruth <- function(setting,
+                     A1_T1,
+                     A1_T2,
+                     A2_T1,
+                     A2_T2,
+                     horizon,
+                     n = 10000,
+                     scale.censored){
+    d = simulateData(setting = setting,
+                     A1_T1 = A1_T1,
+                     A1_T2 = A1_T2,
+                     A2_T1 = A2_T1,
+                     A2_T2 = A2_T2,
+                     n = n,
+                     scale.censored = scale.censored,
+                     keep.latent = TRUE)
+    # average treatment effects in both worlds
+    x <- d[,.(intervene = c("A1","A2","A1","A2","A1","A2","A1","A2"),
+              cause = c(1,1,1,1,2,2,2,2),
+              net = c(1,1,0,0,1,1,0,0),
+              ate = c(
+                  # cause 1: net
+                  mean((T1_treated_placebo <= horizon) - (T1_placebo_placebo <= horizon)),
+                  mean((T1_placebo_treated <= horizon) - (T1_placebo_placebo <= horizon)),
+                  # cause 1: crude
+                  mean(T1_treated_placebo <= pmin(T2_treated_placebo,horizon)) - mean(T1_placebo_placebo <= pmin(T2_placebo_placebo, horizon)),
+                  mean(T1_placebo_treated <= pmin(T2_placebo_treated,horizon)) - mean(T1_placebo_placebo <= pmin(T2_placebo_placebo, horizon)),
+                  # cause 2: net
+                  mean((T2_treated_placebo <= horizon) - (T2_placebo_placebo <= horizon)),
+                  mean((T2_placebo_treated <= horizon) - (T2_placebo_placebo <= horizon)),
+                  # cause 2: crude
+                  mean(T2_treated_placebo <= pmin(T1_treated_placebo,horizon)) - mean(T2_placebo_placebo <= pmin(T1_placebo_placebo, horizon)),
+                  mean(T2_placebo_treated <= pmin(T1_placebo_treated,horizon)) - mean(T2_placebo_placebo <= pmin(T1_placebo_placebo, horizon))),
+              # calculate percent censored before tau
+              scale.censored = scale.censored,
+              censored.tau = round(100*mean(time <= horizon & event == 0),1))]
+    x
+}
+
+######################################################################
+### theTruth.R ends here
